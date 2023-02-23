ab1b1d421b47d1f9ff37d405aa1925cbc2f5c457
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-02-23 12:07:45 +0600

    animated

--- lib/main.dart
+++ lib/main.dart
@@ -20,18 +20,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+double defaultWidth = 100.0;
+
+class _HomePageState extends State<HomePage> {
+  var _isZoomedIn = false;
+  var _buttonTitle = 'Zoom In';
+  var _width = defaultWidth;
+  @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Home Page'),
-      ),
-      body: const Center(
-        child: Text('Home Page'),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 370),
+              curve: Curves.bounceOut,
+              width: _width,
+              child: Image.asset(
+                'assets/images/wallpaper.jpeg',
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _isZoomedIn = !_isZoomedIn;
+                  _buttonTitle = _isZoomedIn ? 'Zoom Out' : 'Zoom In';
+                  _width = _isZoomedIn ? width : defaultWidth;
+                });
+              },
+              child: Text(
+                _buttonTitle,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
